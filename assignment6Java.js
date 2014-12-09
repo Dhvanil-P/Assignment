fc5b1bb76d5beb936b4c2c51eee4f8e1c2797045
Insane- <dhvanil_patel@live.com>
2014-12-08 23:32:17 -0500

    Update assignment6Java.js

--- assignment6Java.js
+++ assignment6Java.js
@@ -1,140 +1,148 @@
 /*Dhvanil Patel
 Dhvanil_patel@live.com
 UMass Lowell in course 91.461 GUI Programming I
-Started: 10/21/2014, last updated: 10/25/2014
+Started: 10/21/2014, last updated: 12/04/2014
 file-name: assignment6Java.js
+I got help from Mihir and Tak and I learned from them.
+Looking forward to learn everything in winter vacation with learnable.
+
 */
-/*function createTable*/
-function createTable() {
-    // using remove to refresh
-    var rowTable = document.getElementById("myTable");
-    if (rowTable != null) rowTable.remove();
-    //gathering values from users and putting them into variables
-    // putting a "+" to make treat it as a number instead of string
-    var numOne = +document.getElementById("numOne").value;
-    var numTwo = +document.getElementById("numTwo").value;
-    var numThree = +document.getElementById("numThree").value;
-    var numFour = +document.getElementById("numFour").value;
-    // get the reference for the body
-    var body = document.getElementsByTagName("body")[0];
-    // check if numbers are validate
-    if (numOne > numTwo) {
-        myFunction();
-        document.getElementById("numOne").setAttribute("style",
-            "outline-color: red");
-        document.getElementById("numOne").setAttribute("style",
-            "background-color: red");
-        return;
-    }
-    if (numThree > numFour) {
-        myFunction();
-        document.getElementById("numThree").setAttribute("style",
-            "outline-color: red");
-        document.getElementById("numThree").setAttribute("style",
-            "background-color: red");
-        return;
-    }
-    // creates a <table> element and a <tableBody> element
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tableBody");
-    // creating all cells
-    for (var i = numOne, ii = numTwo + 1; i <= ii; ++i) {
-        // creates a table row
-        var row = document.createElement("tr");
-        for (var j = numThree, jj = numFour + 1; j <= jj; ++j) {
-            // creates a cell
-            var cell = document.createElement("td");
-            var cellText;
-            // give some style to the cell/table
-            var cellStyle = "padding: 20px; ";
-            if (i == numOne && j == numThree) {
-                cellText = document.createTextNode("");
-                cell.setAttribute("style", cellStyle +
-                    "background-color: white");
-            } else if (i == numOne) {
-                cellText = document.createTextNode(j - 1);
-                cell.setAttribute("style", cellStyle +
-                    "background-color: darkgrey");
-            } else if (j == numThree) {
-                cellText = document.createTextNode(i - 1);
-                cell.setAttribute("style", cellStyle +
-                    "background-color: darkgrey");
-            } else {
-                cellText = document.createTextNode((i - 1) * (j - 1));
-                cell.setAttribute("style", cellStyle +
-                    "background-color: lightgrey");
-            }
-            // add the text to cell
-            cell.appendChild(cellText);
-            // add the cell to row
-            row.appendChild(cell);
-        }
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
-    }
-    // put the <tableBody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tbl);
-    // setting an id for tbl
-    tbl.setAttribute("id", "myTable");
-}
 
-function numbersonly(myfield, e, dec) {
-        var key;
-        var keychar;
-        if (window.event) key = window.event.keyCode;
-        else if (e) key = e.which;
-        else {
+$(document).ready(function() {
+    var tabs = $("#tabs").tabs();
+    /* Validator */
+    $.validator.addMethod('GreaterThanStartRow', function(value, element, param) {
+        if (numTwo.value === "") {
             return true;
         }
-        keychar = String.fromCharCode(key);
-        // control keys
-        if ((key == null) || (key == 0) || (key == 8) || (key == 9) || (key ==
-            13) || (key == 27)) {
-            if (isNormalInteger(myfield.value)) {
-                myfield.setAttribute("style", "outline-color: lightgreen");
-                myfield.setAttribute("style", "border-color: lightgreen");
-            } else {
-                myfield.setAttribute("style", "outline-color: red");
-                myfield.setAttribute("style", "border-color: red");
+        return parseInt(numTwo.value) >= parseInt(numOne.value);
+    }, "The Maximum Column Value must be greater than the Minimum Column Value.");
+    $.validator.addMethod('GreaterThanStartColumn', function(value, element, param) {
+        return parseInt(numFour.value) >= parseInt(numThree.value);
+    }, "The Maximum Row Value must be greater than the Minimum Row value.");
+    $('#form').validate({
+        rules: {
+            numOne: {
+                required: true,
+                digits: true
             }
-            return true;
-        }
-        /*Checking whether inputs are number, if not makes borderline of box red*/
-        else if ((("0123456789").indexOf(keychar) > -1)) {
-            if (isNormalInteger(myfield.value)) {
-                myfield.setAttribute("style", "outline-color: lightgreen");
-                myfield.setAttribute("style", "border-color: lightgreen");
-            } else {
-                myfield.setAttribute("style", "outline-color: red");
-                myfield.setAttribute("style", "border-color: red");
+            numTwo: {
+                required: true,
+                digits: true,
+                GreaterThanStartRow: true
+            },
+            numThree: {
+                required: true,
+                digits: true
+            },
+            numFour: {
+                required: true,
+                digits: true,
+                GreaterThanStartColumn: true
             }
-            return true;
-        }
-        // decimal point jump
-        else if (dec && (keychar == ".")) {
-            myfield.form.elements[dec].focus();
-            myfield.setAttribute("style", "outline-color: red");
-            myfield.setAttribute("style", "border-color: red");
-            return true;
-        } else {
-            if (isNormalInteger(myfield.value)) {
-                myfield.setAttribute("style", "outline-color: lightgreen");
-                myfield.setAttribute("style", "border-color: lightgreen");
+        },
+        onkeyup: function(element) {
+            if ($('form').valid()) {
+                $('form').find(":submit").attr("disabled", false);
             } else {
-                myfield.setAttribute("style", "outline-color: red");
-                myfield.setAttribute("style", "border-color: red");
+                $('form').find(":submit").attr("disabled", true);
             }
-            return true;
+        },
+        /* "The validation plugin allows you to configure these class names"
+         * http://stackoverflow.com/questions/6168926/jquery-validation-how-to-make-fields-red
+         */
+       
+    });
+
+    function crTable(nextTabNo) {
+        /* getting the four values from the user */
+        /* putting a "+" to treat the value as a number instead of string */
+        var numOne = +document.getElementById("numOne").value;
+        var numTwo = +document.getElementById("numTwo").value;
+        var numThree = +document.getElementById("numThree").value;
+        var numFour = +document.getElementById("numFour").value;
+        /* get the reference for the preview */
+        var preview = document.getElementById(nextTabNo);
+        /* creates a <table> element and a <tbody> element */
+        var tbl = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+        /* creating all cells */
+        for (var i = numOne, ii = numTwo + 1; i <= ii; ++i) {
+            /* creates a table row */
+            var row = document.createElement("tr");
+            for (var j = numThree, jj = numFour + 1; j <= jj; ++j) {
+                /* creates a cell */
+                var cell = document.createElement("td");
+                var cellText;
+                /* give some style to the cell/table */
+                var cellStyle = "padding: 10px; color: black;";
+                if (i == numOne && j == numThree) {
+                    cellText = document.createTextNode("");
+                    cell.setAttribute("style", cellStyle + "background-color: white");
+                } else if (i == numOne) {
+                    cellText = document.createTextNode(j - 1);
+                    cell.setAttribute("style", cellStyle + "background-color: purple");
+                } else if (j == numThree) {
+                    cellText = document.createTextNode(i - 1);
+                    cell.setAttribute("style", cellStyle + "background-color: purple");
+                } else {
+                    cellText = document.createTextNode((i - 1) * (j - 1));
+                    cell.setAttribute("style", cellStyle + "background-color: yellow");
+                }
+                /* add the text to cell */
+                cell.appendChild(cellText);
+                /* add the cell to row */
+                row.appendChild(cell);
+            }
+            /* add the row to the end of the table body */
+            tblBody.appendChild(row);
         }
+        /* put the <tbody> in the <table> */
+        tbl.appendChild(tblBody);
+        /* appends <table> into preview */
+        preview.appendChild(tbl);
     }
-    //making a function for pop up for an error
-
-function myFunction() {
-    alert("Please enter a valid input in red box");
-}
-
-function isNormalInteger(str) {
-    return /^\+?(0|[1-9]\d*)$/.test(str);
-}
+     var tabsdiv = $("#tabs");
+    var tabslist = tabsdiv.find("ul");
+    var nextTabNo = tabslist.find("li").length;
+    /* When create button click, a new tab will generate */
+    $('#make').click(function() {
+        /* check for first time */
+        if (!$('form').valid()) {
+            $('form').find(":submit").attr("disabled", true);
+            return;
+        }
+        /* create a new tab with close button next to it
+         * http://stackoverflow.com/questions/14357614/add-close-button-to-jquery-ui-tabs
+         */
+        tabslist.append('<li id="li' + nextTabNo + '"><a href="#tab' + nextTabNo + '">' + 'Tab ' + (nextTabNo + 1) + '<\/a><input name="check" type="checkbox" id="checkbox' +
+            nextTabNo + '"><span id="tabspan' + nextTabNo + '" class="ui-icon ui-icon-circle-close"></span><\/li>');
+        // add content to the new tab */
+        tabsdiv.append('<div id="tab' + nextTabNo + '"><\/div>');
+        /* create content table to the new tab */
+        crTable("tab" + nextTabNo);
+        ++nextTabNo;
+        $('#tabs').tabs("refresh");
+    });
+    /* When close span clicked, it will close the tab that are closest to which you clicked */
+    tabs.delegate("span.ui-icon-circle-close", "click", function() {
+        var panelId = $(this).closest("li").remove().attr("aria-controls");
+        $("#" + panelId).remove();
+        tabs.tabs("refresh");
+    });
+    $('#delete').click(function() {
+        /* push id in the selected */
+        var selected = [];
+        $('input:checkbox:checked').each(function() {
+            selected.push($(this).attr('id'));
+        });
+        /* remove those unwanted tabs */
+        for (var m = 0; m < selected.length; m++) {
+            var checkboxID = "" + selected[m];
+            var num = checkboxID.substring(8, checkboxID.length);
+            $('#tab' + num).remove();
+            $('#li' + num).remove();
+        }
+        $('#tabs').tabs("refresh");
+    });
+});
